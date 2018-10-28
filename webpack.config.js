f7e4da429978d4c61179fcf50e9f065a571e8080
Atsushi Ishida <dolin@Atsushi-no-MacBook-Air.local>
2018-10-28 10:31:41 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: "babel-loader",
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2018 を ES5 に変換
+                "@babel/preset-env",
+                // React の JSX を解釈
+                "@babel/react"
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
