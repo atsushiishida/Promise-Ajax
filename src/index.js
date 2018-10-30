643434dec22f19ab1e89bdacb3b7c645b79af34f
Atsushi Ishida <dolin@Atsushi-no-MacBook-Air.local>
2018-10-30 10:34:52 +0900

    add Promise

--- src/index.js
+++ src/index.js
@@ -1,41 +1,58 @@
+function main(username) {
+  console.log(username);
+  //アプリケーションエントリーポイント
+  getUserInfo(username)
+    .then(userInfo => createView(userInfo))
+    .then(view => displayView(view))
+    .catch(error => {
+      `エラーが発生しました：${error}`;
+    });
+}
+
 function getUserInfo(userId) {
-  const request = new XMLHttpRequest();
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
 
-  request.open("GET", `https://api.github.com/users/atsushiishida`);
+    request.open("GET", `https://api.github.com/users/${userId}`);
 
-  request.addEventListener("load", event => {
-    if (event.target.status !== 200) {
-      console.log(`Error: $(event.target.status):$(event.target.statusText)`);
-      return;
-    }
+    request.addEventListener("load", event => {
+      if (event.target.status !== 200) {
+        console.log(`Error: $(event.target.status):$(event.target.statusText)`);
+        reject();
+      }
 
-    const userInfo = JSON.parse(event.target.responseText);
+      const userInfo = JSON.parse(event.target.responseText);
+      resolve(userInfo);
+    });
+    request.addEventListener("error", () => {
+      console.error("Network Error");
+      reject();
+    });
+    request.send();
+  });
+}
 
-    const view = escapeHTML`
+function createView(userInfo) {
+  return escapeHTML`
 <h4>${userInfo.name} (@${userInfo.login})</h4>
 <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
 <dl>
-    <dt>Location</dt>
-    <dd>${userInfo.location}</dd>
-    <dt>Repositories</dt>
-    <dd>${userInfo.public_repos}</dd>
+  <dt>Location</dt>
+  <dd>${userInfo.location}</dd>
+  <dt>Repositories</dt>
+  <dd>${userInfo.public_repos}</dd>
 </dl>
 `;
-    window.a = view;
-    const result = document.getElementById("result");
-    result.innerHTML = view;
-  });
-  request.addEventListener("error", () => {
-    console.error("Network Error");
-  });
-  request.send();
 }
-// webpackでは、globalスコープに関数を定義しないため、明示的に記載する必要がある
-window.getUserInfo = getUserInfo;
 
-//文字列をエスケープする関数
+function displayView(view) {
+  const result = document.getElementById("result");
+  result.innerHTML = view;
+  window.a = view;
+}
 
 function escapeSpecialChars(str) {
+  //文字列をエスケープする関数
   return str
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
@@ -44,12 +61,13 @@ function escapeSpecialChars(str) {
     .replace(/'/g, "&#039;");
 }
 
-// templateリテラルの処理。
-// stringsには、リテラルの配列
-// ...valuesには、処理された大様式の値が入る
 function escapeHTML(strings, ...values) {
+  /*
+  式の値をエスケープして出力をする。
+  template literalを受け取り、式についてはescapeSpecialChars()関数で処理をする
+
+  */
   return strings.reduce((result, string, i) => {
-    console.log(string);
     const value = values[i - 1];
     if (typeof value === "string") {
       return result + escapeSpecialChars(value) + string;
@@ -58,3 +76,6 @@ function escapeHTML(strings, ...values) {
     }
   });
 }
+
+// webpackでは、globalスコープに関数を定義しないため、明示的に記載する必要がある
+window.main = main;
